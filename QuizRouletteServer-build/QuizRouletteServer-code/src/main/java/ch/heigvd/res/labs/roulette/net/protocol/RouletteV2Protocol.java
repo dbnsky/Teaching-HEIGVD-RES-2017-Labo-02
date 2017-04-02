e7a2b20283d120378b50788d57445410f44e29e6
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-04-02 23:11:26 +0200

    Adding default port to protocol v2

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/RouletteV2Protocol.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/RouletteV2Protocol.java
@@ -9,6 +9,8 @@ public class RouletteV2Protocol extends RouletteV1Protocol {
 
   public final static String VERSION = "2.0";
 
+  public final static int DEFAULT_PORT = 2613;
+
   public final static String CMD_CLEAR = "CLEAR";
   public final static String CMD_LIST = "LIST";
 
