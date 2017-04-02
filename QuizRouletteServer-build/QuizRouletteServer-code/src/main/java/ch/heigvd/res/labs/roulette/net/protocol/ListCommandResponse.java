2198629507606c1ae383700e9737663c324d6f69
dbnsky <emmanuel.schmid@gmail.com>
2017-04-02 16:36:56 +0200

    3 (list, bye, load) classes permettant de communiquer des infos (JSON)

---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/ListCommandResponse.java
@@ -0,0 +1,29 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+import ch.heigvd.res.labs.roulette.data.Student;
+import java.util.List;
+
+/**
+ * This class is used to serialize/deserialize the response sent by the server
+ * when processing the "LIST" command defined in the protocol specification. The
+ * JsonObjectMapper utility class can use this class.
+ *
+ * @author Dbnsky & Iamfonky
+ */
+
+public class ListCommandResponse {
+    private final List<Student> listStudent;
+
+    public ListCommandResponse(List<Student> list) {
+        this.listStudent = list;
+    }
+
+    public List<Student> getList() {
+        return listStudent;
+    }
+
+    public Student getAtList(int index){
+        return listStudent.get(index);
+    }
+
+}
---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/loadCommandResponse.java
@@ -0,0 +1,26 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+/**
+ * This class is used to serialize/deserialize the response sent by the server
+ * when processing the "BYE" command defined in the protocol specification. The
+ * JsonObjectMapper utility class can use this class.
+ *
+ * @author Dbnsky & Iamfonky
+ */
+public class loadCommandResponse {
+    private final String status;
+    private final int numberOfNewStudents;
+
+    loadCommandResponse(String status, int nbNewStudents){
+        this.status = status;
+        this.numberOfNewStudents = nbNewStudents;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public int getNumberOfNewStudents(){
+        return numberOfNewStudents;
+    }
+}
