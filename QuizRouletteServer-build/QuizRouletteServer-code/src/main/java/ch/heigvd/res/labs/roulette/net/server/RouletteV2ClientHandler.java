008ca6edebbdfc18d9ecd0da551d4786775bc54b
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-04-02 17:47:32 +0200

    Correction des objets du protocole

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -3,10 +3,7 @@ package ch.heigvd.res.labs.roulette.net.server;
 import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.IStudentsStore;
 import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
-import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
-import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
-import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
-import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import ch.heigvd.res.labs.roulette.net.protocol.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.io.*;
@@ -20,6 +17,8 @@ import java.util.logging.Logger;
  * @author Olivier Liechti
  */
 public class RouletteV2ClientHandler implements IClientHandler {
+
+  private int nbComands;
 
   final static Logger LOG = Logger.getLogger(RouletteV1ClientHandler.class.getName());
 
@@ -39,22 +38,22 @@ public class RouletteV2ClientHandler implements IClientHandler {
     writer.println("Hello. Online HELP is available. Will you find it?");
     writer.flush();
 
+     nbComands = 0;
+
     String command;
     boolean done = false;
     while (!done && ((command = reader.readLine()) != null)) {
-      LOG.log(Level.INFO, "COMMAND: {0}", command);
+       nbComands++;
+       LOG.log(Level.INFO, "COMMAND: " + nbComands + " : " + command);
+       nbComands++;
       done = v1Manager(command,writer,reader);
       writer.flush();
     }
-
-  
-
-    
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
   
 
-  private boolean v1Manager(String command, PrintWriter writer, BufferedReader reader) throws JsonProcessingException, IOException
+  //On pourrait imaginer placer les méthodes ci-dessous dans un package commun, partagé entre les différents clients handler.
+  private boolean v1Manager(String command, PrintWriter writer, BufferedReader reader) throws IOException
   {
     switch (command.toUpperCase())
     {
@@ -98,23 +97,40 @@ public class RouletteV2ClientHandler implements IClientHandler {
     switch (command.toUpperCase())
     {
       case RouletteV2Protocol.CMD_CLEAR:
+         store.clear();
+         writer.println(RouletteV2Protocol.RESPONSE_CLEAR_DONE);
+         writer.flush();
         break;
+
       case RouletteV2Protocol.CMD_LIST:
-        break;
-      case RouletteV1Protocol.CMD_INFO:
-        InfoCommandResponse response = new InfoCommandResponse(RouletteV1Protocol.VERSION, store.getNumberOfStudents());
-        writer.println(JsonObjectMapper.toJson(response));
-        writer.flush();
-        break;
+         ListCommandResponse listResponse = new ListCommandResponse(store.listStudents());
+         writer.println(JsonObjectMapper.toJson(listResponse));
+         writer.flush();
+         break;
+
       case RouletteV1Protocol.CMD_LOAD:
         writer.println(RouletteV1Protocol.RESPONSE_LOAD_START);
         writer.flush();
+        int nbStudents = store.getNumberOfStudents();
         store.importData(reader);
-        writer.println(RouletteV1Protocol.RESPONSE_LOAD_DONE);
+        nbStudents = store.getNumberOfStudents() - nbStudents;
+        LoadCommandResponse lcr = new LoadCommandResponse("success",nbStudents);
+        writer.println(JsonObjectMapper.toJson(lcr));
         writer.flush();
         break;
-      case RouletteV1Protocol.CMD_BYE:
-        return true;
+
+       case RouletteV1Protocol.CMD_BYE:
+          ByeCommandResponse bcr = new ByeCommandResponse("success",nbComands);
+          writer.println(JsonObjectMapper.toJson(bcr));
+          writer.flush();
+          return true;
+
+       case RouletteV1Protocol.CMD_INFO:
+          InfoCommandResponse icr = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
+          writer.println(JsonObjectMapper.toJson(icr));
+          writer.flush();
+          break;
+
     }
     return false;
   }
